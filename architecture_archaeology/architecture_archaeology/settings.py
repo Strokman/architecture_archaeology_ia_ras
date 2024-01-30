0e434c22870f9d0610d551faf028d47f7d89a8e0
Anton Strokov <anton.strokoff@gmail.com>
2024-01-31 00:23:02 +0300

    assada

--- architecture_archaeology/architecture_archaeology/settings.py
+++ architecture_archaeology/architecture_archaeology/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', 'base-habilis.ru']
 
 # Application definition
 
