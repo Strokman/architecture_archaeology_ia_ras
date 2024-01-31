9146f78920ad15a9736f996471e383a0cff846dd
Anton Strokov <anton.strokoff@gmail.com>
2024-01-31 16:26:16 +0300

    try

--- architecture_archaeology/boot.sh
+++ architecture_archaeology/boot.sh
@@ -3,4 +3,4 @@
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic
 python3 manage.py loaddata countries regions building_parts colors buildings filetypes materials pigments preservations storages arch_sites
-gunicorc architecture_archaeology.wsgi --bind 0.0.0.0:8000 --log-level debug
+gunicorn architecture_archaeology.wsgi --bind 0.0.0.0:8000 --log-level debug
