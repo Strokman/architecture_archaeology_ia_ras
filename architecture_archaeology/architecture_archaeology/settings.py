c6ca492deb9dbf8fa6f69ab8740660592cd48560
Anton Strokov <anton.strokoff@gmail.com>
2024-01-31 16:43:29 +0300

    testsfafs

--- architecture_archaeology/architecture_archaeology/settings.py
+++ architecture_archaeology/architecture_archaeology/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
