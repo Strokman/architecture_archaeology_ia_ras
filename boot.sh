6cb264f9eb525fcd4d5c4f71a1a89fd1a3a8461c
Anton Strokov <anton.strokoff@gmail.com>
2024-02-07 08:59:54 +0300

    test

--- boot.sh
+++ boot.sh
@@ -3,6 +3,6 @@
 # python3 manage.py migrate --noinput
 # python3 manage.py collectstatic
 # python3 manage.py loaddata countries regions building_parts colors buildings filetypes materials pigments preservations storages arch_sites
-python3 manage.py migrate
-python3 manage.py loaddata filetypes minerals
+# python3 manage.py migrate
+# python3 manage.py loaddata filetypes minerals
 gunicorn architecture_archaeology.wsgi --bind 0.0.0.0:8000 --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --log-level debug
