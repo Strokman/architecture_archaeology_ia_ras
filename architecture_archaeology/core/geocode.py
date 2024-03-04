381cbce1fe2a508b655c684bb9a9905f5b02111f
Anton Strokov <anton.strokoff@gmail.com>
2024-03-04 23:06:18 +0300

    geocode improvement

--- architecture_archaeology/core/geocode.py
+++ architecture_archaeology/core/geocode.py
@@ -1,9 +1,9 @@
 import requests
 import architecture_archaeology.settings as settings
 from decimal import Decimal
-from pprint import pprint
 
-def create_geocode_url(long, lat):
+
+def create_geocode_url(lat, long):
     if not isinstance(long, (Decimal, float)) and not isinstance(lat, (Decimal, float)):
         raise ValueError('Not a decimal value')
     url = f'https://geocode-maps.yandex.ru/1.x?apikey={settings.YMAPS_TOKEN}&geocode={long}, {lat}&lang=ru_RU&format=json'
@@ -11,21 +11,16 @@ def create_geocode_url(long, lat):
 
 
 def get_location_data(url):
-    resp = requests.get(url).json()
-    print(resp)
-    # for i in resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']:
-    #     print(i)
-    print('------------')
-    # print(resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components'])
-    # return resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
-    # try:
-    #     address_data = resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
-    #     region_data = {
-    #         "country": address_data[0]['name'],
-    #         "region": address_data[2]['name'],
-    #         "rayon": address_data[3]['name'],
-    #         "locality": address_data[-1]['name']
-    #     }
-    #     # print(region_data)
-    # except:
-    #     print(resp['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components'])
+    resp = requests.get(url).json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject']['metaDataProperty']['GeocoderMetaData']['Address']['Components']
+    region_data = {}
+    for i in resp:
+        match i['kind']:
+            case 'country':
+                region_data['country'] = i['name']
+            case 'province':
+                region_data['region'] = i['name']
+            case 'area' if 'region' not in region_data:
+                region_data['region'] = i['name']
+            case 'locality' if 'region' not in region_data:
+                region_data['region'] = i['name']
+    return region_data
