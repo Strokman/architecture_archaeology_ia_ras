c80a4ef7bc4656cac50b4a301be5d4000cb210da
Anton Strokov <anton.strokoff@gmail.com>
2024-03-10 21:59:46 +0300

    js script fix

--- architecture_archaeology/static/js/map.js
+++ architecture_archaeology/static/js/map.js
@@ -4,52 +4,39 @@ ymaps.ready(function(){
         zoom: 6,
         controls: ['rulerControl', 'searchControl', "zoomControl", "typeSelector", "searchControl"]
     });
-    
-    fetch('/map/get/').then(function(response) {
-          response.json().then(function(data) {
-            data.forEach(element => {
-                var myGeoObject = new ymaps.Placemark([element.fields.lat, element.fields.long], {
-                    // Задаем содержимое балуна метки.
-                    // Это содержимое будет отображаться при клике по кластеру.
-                    balloonContentHeader: element.fields.name,
-                    balloonContentBody: element.fields.description +'<br>' + '<a href="/arch-site/detail/' + element.fields.slug + '/">Карточка объекта</a>',
-                    hintContent: element.fields.name
+    var marks = fetch('/map/get/').then(function(response) {
+        return response.json();
+    });
+    function renderMarks(data){
+        data.then(function(data) {
+                data.forEach(element => {
+                    var myGeoObject = new ymaps.Placemark([element.fields.lat, element.fields.long], {
+                        // Задаем содержимое балуна метки.
+                        // Это содержимое будет отображаться при клике по кластеру.
+                        balloonContentHeader: element.fields.name,
+                        balloonContentBody: element.fields.description +'<br>' + '<a href="/arch-site/detail/' + element.fields.slug + '/">Карточка объекта</a>',
+                        hintContent: element.fields.name
+                    });
+                    map.geoObjects.add(myGeoObject);
                 });
-                map.geoObjects.add(myGeoObject);
-            });
- 
-            });
+    
+                });
   
-  });
-      map.events.add('boundschange', function (event) {
+        };
+    renderMarks(marks);
+    map.events.add('boundschange', function (event) {
         var currentZoom = event.get('newZoom');
 
         // Проверяем текущий масштаб
         if (currentZoom >= 10) {
-            // Добавляем метки или что-то делаем, если масштаб меньше 10
-            // В данном примере просто добавим метки на карту
+            // Удаляем метки, если зум больше или равно 10
             map.geoObjects.removeAll();
         } else {
-            // Убираем метки, если масштаб больше или равен 10
-                
-    fetch('/map/get/').then(function(response) {
-        response.json().then(function(data) {
-          data.forEach(element => {
-              var myGeoObject = new ymaps.Placemark([element.fields.lat, element.fields.long], {
-                  // Задаем содержимое балуна метки.
-                  // Это содержимое будет отображаться при клике по кластеру.
-                  balloonContentHeader: element.fields.name,
-                  balloonContentBody: element.fields.description +'<br>' + '<a href="/arch-site/detail/' + element.fields.slug + '/">Карточка объекта</a>',
-                  hintContent: element.fields.name
-              });
-              map.geoObjects.add(myGeoObject);
-          });
+            // Возвращаем метки на место
 
-          });
-
-});
+            renderMarks(marks);
         }
     });
-    });
+});
 
 
