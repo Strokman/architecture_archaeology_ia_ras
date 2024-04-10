df914d0343d18bd3dcbee634e10780ef1fd096cc
Anton Strokov <anton.strokoff@gmail.com>
2024-04-10 15:58:04 +0200

    fix script

--- boot.sh
+++ boot.sh
@@ -2,6 +2,5 @@
 
 python3 manage.py migrate --noinput
 python3 manage.py collectstatic
-python3 manage.py loaddata all
-# python3 manage.py migrate
+# python3 manage.py loaddata all
 gunicorn architecture_archaeology.wsgi --bind 0.0.0.0:8000 --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --log-level debug
