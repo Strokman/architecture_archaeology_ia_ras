a731582e8c28afc5d1afa3b99ea2e23496fb9141
Anton Strokov <anton.strokoff@gmail.com>
2024-04-15 15:18:05 +0200

    docker compose fix

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python3 manage.py migrate --noinput
-python3 manage.py collectstatic
+# python3 manage.py collectstatic
 # python3 manage.py loaddata all
 gunicorn architecture_archaeology.wsgi --bind 0.0.0.0:8000 --access-logfile - --error-logfile - --capture-output --enable-stdio-inheritance --log-level debug
