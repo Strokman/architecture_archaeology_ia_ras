1934bca330eb0bbfbef06890fabaab5fed8f0859
Anton Strokov <anton.strokoff@gmail.com>
2024-05-06 10:57:32 +0200

    md test

--- architecture_archaeology/core/models/__init__.py
+++ architecture_archaeology/core/models/__init__.py
@@ -1,5 +1,5 @@
+from .base_model import BaseModel
 from .description_mixin import DescriptionMixin
 from .slug_mixin import SlugMixin
 from .timestamp_mixin import TimestampMixin
-from .base_model import BaseModel
 from .year_mixin import YearMixin
