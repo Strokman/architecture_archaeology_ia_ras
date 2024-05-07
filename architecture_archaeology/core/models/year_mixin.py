a770cefe7864164a1b92aaa7ee16c8773b2b31c5
Anton Strokov <anton.strokoff@gmail.com>
2024-05-07 11:44:18 +0200

    test

--- architecture_archaeology/core/models/year_mixin.py
+++ architecture_archaeology/core/models/year_mixin.py
@@ -4,20 +4,11 @@ from core.models.base_model import BaseModel
 
 class YearMixin(BaseModel):
 
-    year_min = models.IntegerField(
-        null=True,
-        blank=True,
-        verbose_name='Датировка от:'
-        )
-    year_max = models.IntegerField(
-        null=True,
-        blank=True,
-        verbose_name='до:',
-        help_text='Даты до н.э. должны быть отрицательными'
-        )
+    year_min = models.IntegerField(null=True, blank=True, verbose_name='Датировка от:')
+    year_max = models.IntegerField(null=True, blank=True, verbose_name='до:', help_text='Даты до н.э. должны быть отрицательными')
 
     def dating(self):
-        a = ('г. до н.э.', 'г.')
+        a = ('', 'г. н.э.')
         year_min = ''
         year_max = ''
         date = ''
@@ -31,12 +22,10 @@ class YearMixin(BaseModel):
             date = f'Не ранее {year_min}'
         elif self.year_max:
             date = f'Не позднее {year_max}'
-        if self.year_min > 0 and self.year_max > 0:
-            date = date.replace('г.', '', 1)
-            date = date.replace('г.', 'гг.')
-        elif self.year_min < 0 and self.year_max < 0:
-            date = date.replace('г. до н.э.', '', 1)
-            date = date.replace('г.', 'гг.')
+        for i in a:
+            if date.count(i) > 1:
+                date = date.replace(i, '', 1)
+                date = date.replace(i, f'г{i}')
         return date
 
     class Meta:
