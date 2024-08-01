e2f9c1359544c6761116d22c4dbf0fd1b31adb72
Anton Strokov <anton.strokoff@gmail.com>
2024-08-01 14:55:46 +0300

    add colors to db records

---
+++ architecture_archaeology/helpers/management/commands/fixcolors.py
@@ -0,0 +1,28 @@
+from django.core.management.base import BaseCommand
+from architecture_archaeology.settings import BASE_DIR
+from helpers.models import Color
+from artwork.models import Frescoe
+from csv import DictReader
+
+
+
+class Command(BaseCommand):
+    help = "Привязка цветов из файла"
+
+    def handle(self, *args, **options):
+        with open(f'{BASE_DIR}/pre_defined_data/frescoes_main.csv', 'r', encoding='utf-8-sig') as f:
+            data = DictReader(f, delimiter=';')
+            
+            for row in data:
+                if row['№']:
+                    try:
+                        color = row['цвет'].split()
+                        frescoe = Frescoe.objects.get(code=row['№'])
+                        if color:
+                            for i in color:
+
+                                db_color = Color.objects.get(description=i)
+                                frescoe.color.add(db_color)
+                                frescoe.save()
+                    except KeyError:
+                        pass
